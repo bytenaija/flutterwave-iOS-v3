c11ca67e6d5b2f2373a52bc9ab9dea98cf9082cd
Everistus Olumese <everistusolumese@gmail.com>
2024-02-02 14:22:52 -0500

    Added Error handling WebviewPay

--- FlutterwaveSDK/Classes/UI/FlutterwavePayWebViewController.swift
+++ FlutterwaveSDK/Classes/UI/FlutterwavePayWebViewController.swift
@@ -60,7 +60,7 @@ class RavePayWebViewController: UIViewController, WKNavigationDelegate,WKUIDeleg
         PaymentServicesViewModel.sharedViewModel.error.observeOn(MainScheduler.instance).subscribe(onNext: { error in
             self.progressView.removeFromSuperview()
             showSnackBarWithMessage(msg: error )
-            self.delegate?.transactionFailed(nil, nil)
+            self.delegate?.transactionFailed(flwRef: nil, nil)
         } ).disposed(by: diposableBag)
     }
 
